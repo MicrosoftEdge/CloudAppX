0cd7af0ea73dd74ca322bb96ab7bb3d4b8ff116f
Fernando Tubio <fernando.tubio@southworks.net>
2015-07-20 15:53:19 -0300

    Added detection of local Windows 10 Kit installation of makeappx with fallback to local copy in 'appxsdk' subfolder

--- server/lib/build.js
+++ server/lib/build.js
@@ -7,6 +7,9 @@ var path = require('path');
 var unzip2 = require('unzip2');
 var os = require('os');
 
+var defaultToolsFolder = '.\\appxsdk\\';
+var packageName = 'package.appx';
+
 function getappx(file) {
   var deferred = Q.defer();
   getContents(file.xml)
@@ -17,27 +20,73 @@ function getappx(file) {
   return deferred.promise;
 }
 
+function getLocalToolPath(toolname) {
+  var deferred = Q.defer();
+  var cmdLine = 'powershell -Command "Get-ItemProperty \\"HKLM:\\SOFTWARE\\Microsoft\\Windows Kits\\Installed Roots\\" -Name KitsRoot10 | Select-Object -ExpandProperty KitsRoot10"';
+  exec(cmdLine, function (err, stdout, stderr) {
+    if (err) {
+      console.log(err);
+      return deferred.reject(new Error('Failed to retrieve the Windows 10 Kit path.'));
+    } else if (stderr.length) {
+      console.log(stderr.trim());
+    }
+    
+    var toolPath = path.resolve(stdout.replace(/[\n\r]/g, ''), 'bin', os.arch(), toolname);
+    fs.exists(toolPath, function (exists) {
+      if (exists) {
+        return deferred.resolve(toolPath);
+      }
+      
+      toolPath = path.resolve(__dirname, defaultToolsFolder + toolname);
+      fs.exists(toolPath, function (exists) {
+        if (!exists) {
+          toolPath = undefined;
+        }
+        
+        return deferred.resolve(toolPath);
+      });
+    });
+  });
+  
+  return deferred.promise;
+};
+
 function makeappx(file) {
   var deferred = Q.defer();
-  var cmdLine = 'touch ' + path.join(file.out, file.name + '.appx');
   if (os.platform() === 'win32') {
-    cmdLine = '.\\appxsdk\\makeappx.exe pack /o /d ' + file.dir + ' /p ' + file.out + '\\Package.appx /l /v';
+    getLocalToolPath('makeappx.exe').then(
+      function (toolpath) {
+        cmdLine = undefined;
+        if (toolpath) {
+          cmdLine = '"' + toolpath + '" pack /o /d ' + file.dir + ' /p ' + file.out + '\\' + packageName + ' /l /v';
+          console.log(cmdLine);
+          exec(cmdLine, function (err, stdout, stderr) {
+            if (err) {
+              deferred.reject(err);
+            }
+            
+            console.log(stdout);
+            console.log(stderr);
+            var output = {
+              name: path.join(file.dir, file.name + '.appx'),
+              out: path.join(file.out, packageName),
+              stdout: stdout,
+              stderr: stderr
+            };
+            
+            deferred.resolve(output);
+          });
+        }
+      },
+      function (err) {
+        deferred.reject(err);
+      });
+  } else {
+    deferred.reject(new Error('Cannot generate a Windows Store package in the current platform.'));
   }
-  console.log(cmdLine);
-  exec(cmdLine, function(err, stdout, stderr) {
-    console.log(stdout);
-    console.log(stderr);
-    var output = {
-      name: path.join(file.dir, file.name + '.appx'),
-      out: path.join(file.out, 'Package.appx'),
-      stdout: stdout,
-      stderr: stderr
-    };
-    deferred.resolve(output);
-  });
-
+  
   return deferred.promise;
-}
+};
 
 function getContents(file) {
   var deferred = Q.defer();
